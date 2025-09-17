ba2fab204aaa059ce2cc9dfabdf57eb3999fbf8b
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-17 19:19:17 +0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Saira } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const saira = Saira({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${saira.className} antialiased bg-navbar container-mx-auto`}
       >
-        {children}
+        <Navbar />
+        <main className="">
+          {children}
+        </main>
       </body>
     </html>
   );
