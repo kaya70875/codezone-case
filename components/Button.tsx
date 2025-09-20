5808000c07c9f1b766c307e8da8a1c7e131536f3
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-20 16:08:21 +0300

    update Button component to keep default classnames

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,17 +3,18 @@ import React from 'react'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     variant?: 'default' | 'ghost';
+    className?: string;
 }
 
-export default function Button({ children, variant = 'default', ...props }: ButtonProps) {
+export default function Button({ children, variant = 'default', className, ...props }: ButtonProps) {
 
     const variants = {
         default: 'bg-white text-navbar uppercase font-[700]',
-        ghost: 'border-white text-white bg-navbar'
+        ghost: 'text-white bg-navbar border border-gray-200'
     }
 
     return (
-        <button className={`cursor-pointer px-7 py-2 flex items-center justify-center ${variants[variant]}`} {...props}>
+        <button className={`cursor-pointer px-7 py-2 flex items-center justify-center ${variants[variant]} ${className}`} {...props}>
             {children}
         </button>
     )
